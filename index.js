de13d23ef6f7e4b307a1fb955a5290cbc6a74644
Bogdan Stanga <bogydaci@gmail.com>
2018-11-21 16:36:56 +0200

    created crash and wait routes

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res) {
+    res.send("Working");
+});
+
+app.get("/crash", function(req, res) {
+    process.nextTick(function() {
+        throw new Error();
+    });
+    res.send("I think it crashed");
+});
+
+app.get("/wait", function(req, res) {
+    setTimeout(() => {
+        res.send("Finally");
+    }, 10000);
+});
+
+app.listen(8080, function() {
+    console.log("Listening on port:" + 8080);
+});
