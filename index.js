16a23930314b0ac8adec1c35e7f8cc29e151e035
Bogdan Stanga <bogydaci@gmail.com>
2018-12-22 12:30:28 +0200

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res) {
-    res.send("Working 20");
+    res.send("Working 21");
 });
 
 app.get("/crash", function(req, res) {
